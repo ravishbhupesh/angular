b1b5ae10b13e7c7ceabead14849e07facf38167c
Bhupesh <ravishbhupesh@gmail.com>
2017-11-25 16:49:15 +0100

    angular form created

--- bookingbot/src/app/app.module.ts
+++ bookingbot/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FlightComponent } from './flight/flight.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightComponent
   ],
   imports: [
     BrowserModule
---
+++ bookingbot/src/app/flight-search.ts
@@ -0,0 +1,16 @@
+export class FlightSearch {
+  public origin: string;
+  public destination: string;
+  public depDate: Date;
+  public rtrnDate: Date;
+  public travelType: string;
+  public directFlightsOnly: boolean;
+  public refundable: boolean;
+  public adults: number;
+  public chidren: number;
+  public infants: number;
+
+  constructor() {}
+
+}
+
